20d0b18ff4d4b03ecd0bc5e17bd60a14a7b9aade
Meerten3 <70748821+Meerten3@users.noreply.github.com>
2022-05-19 09:41:31 +0200

    Alle paginas aangemaakt

--- app/src/main/java/com/example/approbotica/RobotActivity.java
+++ app/src/main/java/com/example/approbotica/RobotActivity.java
@@ -2,7 +2,10 @@ package com.example.approbotica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class RobotActivity extends AppCompatActivity {
 
@@ -11,5 +14,60 @@ public class RobotActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_robot);
         getSupportActionBar().hide();
+        configureBackButton();
+        configureButton1();
+        configureButton2();
+        configureButton3();
+        configureButton4();
+    }
+
+    private void configureBackButton(){
+        ImageView ImageButton = (ImageView) findViewById(R.id.BackButton);
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void configureButton1(){
+        ImageView ImageButton = (ImageView) findViewById(R.id.ButtonImage1);
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RobotActivity.this, DanceActivity.class));
+            }
+        });
+    }
+
+    private void configureButton2(){
+        ImageView ImageButton = (ImageView) findViewById(R.id.ButtonImage2);
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RobotActivity.this, SeedActivity.class));
+            }
+        });
+    }
+
+    private void configureButton3(){
+        ImageView ImageButton = (ImageView) findViewById(R.id.ButtonImage3);
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RobotActivity.this, CameraActivity.class));
+            }
+        });
+    }
+
+    private void configureButton4(){
+        ImageView ImageButton = (ImageView) findViewById(R.id.ButtonImage4);
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RobotActivity.this, ViewActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/approbotica/SeedActivity.java
@@ -0,0 +1,14 @@
+package com.example.approbotica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seed);
+    }
+}
