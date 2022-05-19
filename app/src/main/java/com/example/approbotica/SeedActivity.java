e237873c60508702e5971a05b6827136bb0fa727
Meerten3 <70748821+Meerten3@users.noreply.github.com>
2022-05-19 12:04:30 +0200

    Alle pagina's op view zijn af

--- app/src/main/java/com/example/approbotica/SeedActivity.java
+++ app/src/main/java/com/example/approbotica/SeedActivity.java
@@ -3,6 +3,8 @@ package com.example.approbotica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SeedActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seed);
+        getSupportActionBar().hide();
+        configureBackButton();
+    }
+
+    private void configureBackButton(){
+        ImageView ImageButton = (ImageView) findViewById(R.id.BackButton);
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
