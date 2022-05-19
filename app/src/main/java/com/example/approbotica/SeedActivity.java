5715ac19c0e4c3914558ca2aa2d7fb04a9ea9555
Meerten3 <70748821+Meerten3@users.noreply.github.com>
2022-05-19 12:35:39 +0200

    Revert "Alle pagina's op view zijn af"
    This reverts commit e237873c60508702e5971a05b6827136bb0fa727.

--- app/src/main/java/com/example/approbotica/SeedActivity.java
+++ app/src/main/java/com/example/approbotica/SeedActivity.java
@@ -3,8 +3,6 @@ package com.example.approbotica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 public class SeedActivity extends AppCompatActivity {
 
@@ -12,17 +10,5 @@ public class SeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seed);
-        getSupportActionBar().hide();
-        configureBackButton();
-    }
-
-    private void configureBackButton(){
-        ImageView ImageButton = (ImageView) findViewById(R.id.BackButton);
-        ImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
     }
 }
